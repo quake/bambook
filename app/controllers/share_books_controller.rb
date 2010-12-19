3d7b32a97adc03d6a8eefdad492e884adfea1f04
quake wang <quake.wang@gmail.com>
2010-12-19 20:44:48 +0800

    add plugin check and ui tweak

--- app/controllers/share_books_controller.rb
+++ app/controllers/share_books_controller.rb
@@ -1,6 +1,6 @@
 class ShareBooksController < ApplicationController
   def index
-    @books = ShareBook.paginate(:page => params[:page], :order => "download_count desc")
+    @books = ShareBook.paginate(:page => params[:page], :order => "#{params[:sort] == 'time' ? 'created_at' : 'download_count'} desc")
     render :action => :index, :layout => false
   end
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,16 +6,25 @@ $.template("bambook_book_template", "<tr guid='${guid}'><td>${name}</td><td>${au
 
 function refreshBambookBooks() {
     $('#server_books').hide();
-    $('#bambook_books').show();
-    popupMessage("正在获取Bambook上的自有书籍信息");
+    if(bb.valid) {
+        if(bb.getConnectStatus() == 0) {
+            $('#bambook_books').show();
+            popupMessage("正在获取Bambook上的自有书籍信息");
 
-    var bambook_books = $("#bambook_books tbody")
-    bambook_books.empty();
-    $.tmpl("bambook_book_template", bb.getPrivBookInfos()).appendTo(bambook_books);
-    hideMessage();
+            var bambook_books = $("#bambook_books tbody")
+            bambook_books.empty();
+            $.tmpl("bambook_book_template", bb.getPrivBookInfos()).appendTo(bambook_books);
+            hideMessage();
+        }else{
+            $("#box").show();
+        }
+    }else{
+        $("#install_plugin").show();
+    }
 }
 
 function refreshServerBooks(url) {
+    $('#box').hide();
     $('#bambook_books').hide();
     $('#server_books').show();
     popupMessage("正在获取服务器上的数据");
@@ -33,7 +42,7 @@ function connect() {
     var result = bb.connect($("#bambook_ip").val());
     if(result == 0) {
         sn = bb.getDeviceInfo()["sn"];
-        $('#box').remove();
+        $('#box').hide();
         refreshBambookBooks();
     }else{
         $('#message').html("连接错误: " + result);
@@ -106,7 +115,7 @@ $(function() {
     });
 
     $("#bambook_books a.delete").live('click', function() {
-        if(confirm("你确定要在Bambook上删除这本书吗？")) {
+        if(confirm("您确定要在Bambook上删除这本书吗？")) {
             bb.deletePrivBook($(this).parent().parent().attr("guid"));
             refreshBambookBooks();
             return false;
@@ -124,17 +133,22 @@ $(function() {
     });
 
     $("#server_books a.download-share").live('click', function() {
-        popupMessage("正在从服务器下载");
-        var sid = $(this).parent().parent().attr("sid");
-        $.get("/share_books/" + sid,
-            function(data){
-                bb.addPrivBookByRawData("temp.snb", data);
-            });
+        if(sn == "") {
+            popupMessage("您需要先连接到Bambook才能下载");
+            hideMessage();
+        }else{
+            popupMessage("正在从服务器下载");
+            var sid = $(this).parent().parent().attr("sid");
+            $.get("/share_books/" + sid,
+                function(data){
+                    bb.addPrivBookByRawData("temp.snb", data);
+                });
+        }
         return false;
     });
 
     $("#server_books a.delete").live('click', function() {
-        if(confirm("你确定要在服务器上删除这本书吗？")) {
+        if(confirm("您确定要在服务器上删除这本书吗？")) {
             var container = $(this).parent().parent();
             $.post("/books/" + container.attr("sid") + "?sn=" + sn, {
                 "_method": "delete"
@@ -158,38 +172,41 @@ $(function() {
     });
 
     $("#main-navigation a").click(function(){
-        if(sn != "") {
-            $("#main-navigation li").removeClass("active");
-            $(this).parent().addClass("active");
-            switch ($(this).attr("id")) {
-                case "my_server_books":
-                    refreshServerBooks("/books");
-                    break;
-                case "my_bambook_books":
-                    refreshBambookBooks();
-                    break;
-                case "other_shares":
-                    refreshServerBooks("/share_books");
-                    break;
-                case "subscriptions":
-                    refreshServerBooks("/subscriptions");
-                    break;
-                default:
-                    break;
-            }
-        }else{
-            return false;
+        $("#main-navigation li").removeClass("active");
+        $(this).parent().addClass("active");
+        switch ($(this).attr("id")) {
+            case "my_server_books":
+                refreshServerBooks("/books");
+                break;
+            case "my_bambook_books":
+                refreshBambookBooks();
+                break;
+            case "other_shares":
+                refreshServerBooks("/share_books");
+                break;
+            case "subscriptions":
+                refreshServerBooks("/subscriptions");
+                break;
+            default:
+                break;
         }
-    });
-
-    $("div.pagination a").live('click', function() {
-        $("#server_books tbody").load($(this).attr("href"));
         return false;
     });
+
+    $("div.pagination a, #server_books ul a").live('click', function() {
+        $("#server_books").load($(this).attr("href"));
+        return false;
+    });
+
+    refreshBambookBooks();
 });
 
 $(window).unload(function() {
-    bb.disconnect();
+    try{
+        bb.disconnect();
+    }catch(e){
+    //ignore;
+    }
 });
 
 $.ajaxSetup({
